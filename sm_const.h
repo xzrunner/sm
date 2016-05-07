d67f3248e48105603c08591396c2c8f53d10385a
zz <zhuguang@ejoy.com>
2016-05-07 17:40:46 +0800

    [FIXED] const

--- sm_const.h
+++ sm_const.h
@@ -6,10 +6,17 @@ extern "C"
 #ifndef spatial_math_const_h
 #define spatial_math_const_h
 
+#ifdef __cplusplus
 static const float SM_PI = 3.1415926f;
+#else
+#define SM_PI 3.1415926f
+#endif // __cplusplus
+static const float SM_TWO_PI = 2 * SM_PI;
 
-static const float SM_DEG_TO_RAD = 3.1415926f / 180.0f;
-static const float SM_RAD_TO_DEG = 3.1415926f * 180.0f;
+static const float SM_DEG_TO_RAD = SM_PI / 180.0f;
+static const float SM_RAD_TO_DEG = SM_PI * 180.0f;
+
+static const float SM_LARGE_EPSILON = 0.001f;
 
 #endif // spatial_math_const_h
 

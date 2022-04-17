62d9604478e6959d1cab5ec8c9f76ceca5492bfd
zz <xzrunner@gmail.com>
2022-04-18 07:45:02 +0800

    cleanup

--- SM_Calc.cpp
+++ SM_Calc.cpp
@@ -7,7 +7,7 @@ sm::vec3 calc_plane_mirror(const Plane& plane, const sm::vec3& pos)
 {
 	float len_s = plane.normal.LengthSquared();
 	auto v1 = pos;
-	float k = (-plane.normal.Dot(v1) - plane.dist) / len_s;
+	float k = - plane.GetDistance(v1) / len_s;
 	auto v2 = plane.normal * k + v1;
 	return v2 * 2 - v1;
 }

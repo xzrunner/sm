9dee36e82ac06e062d7aaba3385f6761c264f80d
zz <xzrunner@gmail.com>
2022-07-02 17:34:53 +0800

    [FIXED] expand polygon, border and holes, check input order

--- SM_Polyline.cpp
+++ SM_Polyline.cpp
@@ -1,4 +1,5 @@
 #include "SM_Polyline.h"
+#include "SM_Test.h"
 
 #include <cavc/polylineoffset.hpp>
 
@@ -33,22 +34,48 @@ polyline_offset(const std::vector<vec2>& polyline, float distance, bool is_close
 }
 
 std::vector<std::vector<vec2>>
-polyline_expand(const std::vector<vec2>& polyline, float offste, bool is_closed)
+polyline_expand(const std::vector<vec2>& polyline, float distance, bool is_closed)
 {
 	std::vector<std::vector<vec2>> ret;
 
-	auto off0 = polyline_offset(polyline, offste, is_closed);
-	auto off1 = polyline_offset(polyline, -offste, is_closed);
-
-	if (off0.size() != off1.size() || off0.empty()) {
-		return ret;
-	}
-
-	for (int i = 0, n = off0.size(); i < n; ++i)
+	if (is_closed)
 	{
-		auto off(off0[i]);
-		std::copy(off1[i].rbegin(), off1[i].rend(), std::back_inserter(off));
-		ret.push_back(off);
+		std::vector<std::vector<vec2>> border, holes;
+		if (!is_polygon_clockwise(polyline))
+		{
+			auto fixed = polyline;
+			std::reverse(fixed.begin(), fixed.end());
+			border = polyline_offset(fixed, distance, is_closed);
+			holes = polyline_offset(fixed, -distance, is_closed);
+		}
+		else
+		{
+			border = polyline_offset(polyline, distance, is_closed);
+			holes = polyline_offset(polyline, -distance, is_closed);
+		}
+
+		assert(border.size() == 1);
+		ret.push_back(border[0]);
+
+		for (auto& hole : holes) {
+			ret.push_back(hole);
+		}
+	}
+	else
+	{
+		auto off0 = polyline_offset(polyline, distance, is_closed);
+		auto off1 = polyline_offset(polyline, -distance, is_closed);
+
+		if (off0.size() != off1.size() || off0.empty()) {
+			return ret;
+		}
+
+		for (int i = 0, n = off0.size(); i < n; ++i)
+		{
+			auto off(off0[i]);
+			std::copy(off1[i].rbegin(), off1[i].rend(), std::back_inserter(off));
+			ret.push_back(off);
+		}
 	}
 
 	return ret;
